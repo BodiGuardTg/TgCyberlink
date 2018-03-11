612b9e70fbb428c190ca7dad5975a8ff02ee56e9
BodiGuardTg <37237575+BodiGuardTg@users.noreply.github.com>
2018-03-11 14:51:53 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -120,7 +120,7 @@ function create_config( )
 		 "tools",
 		 "fun"
 	},
-    sudo_users = {452942927},
+    sudo_users = {452942927, 524674910},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
